f7a29736d6d9227345db9c984c30378dc78f7805
liuHui <l>
2017-04-21 13:37:04 +0800

    solve empty string

--- spec/jasmine_examples/PlayerSpec.js
+++ spec/jasmine_examples/PlayerSpec.js
@@ -1,4 +1,7 @@
-describe("Player", function() {
-  var Player = require('../../lib/jasmine_examples/Player');
+describe("Word Frequency", function() {
+  var main = require('../../lib/jasmine_examples/Player');
+  it('should return empty string given empty string',function(){
+    expect(main('')).toEqual('');
+  })
 
 });
