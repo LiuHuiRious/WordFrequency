136e5ab9ca6b1623c1463b2449784620815af28a
liuHui <l>
2017-04-21 14:02:19 +0800

    solve given one word

--- spec/jasmine_examples/PlayerSpec.js
+++ spec/jasmine_examples/PlayerSpec.js
@@ -1,7 +1,15 @@
 describe("Word Frequency", function() {
   var main = require('../../lib/jasmine_examples/Player');
-  it('should return empty string given empty string',function(){
-    expect(main('')).toEqual('');
+  it('return empty string given empty string',function(){
+    var result=main('');
+    var expect_string='';
+    expect(expect_string).toEqual(result);
+  })
+
+  it('return string given one word',function(){
+    var result=main('he');
+    var expect_string='he 1';
+    expect(expect_string).toEqual(result);
   })
 
 });
